c2c0a46f88fabf6c97617cd8a407a80c1122274f
YaninaCastro <yanicastrouni@gmail.com>
2026-05-29 01:28:52 -0300

    Bloque 2 completo - hooks y ListaProductos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-<TarjetaProducto v-for="producto in productos" :key="producto.id">
-    <template #header>{{ producto.nombre }}-{{ producto.categoria }}</template>
-    <template #body="{expandida, toggleExpandir}">
-        <button @click="toggleExpandir">{{ expandida ? 'Cerrar':'Ver más' }}</button>
-        <p v-if="expandida">Este producto tiene información adicional disponible</p>
-    </template>
-</TarjetaProducto>
+<ListaProductos :productos="productos"/>
 </template>
 
 <script setup>
-import TarjetaProducto from './components/TarjetaProducto.vue';
+import ListaProductos from './components/ListaProductos.vue';
 import { productos } from './data/productos.js';
+
+
 </script>
 
 
--- src/components/TarjetaProducto.vue
+++ src/components/TarjetaProducto.vue
@@ -1,22 +1,24 @@
 <template>
-<div class="header"><slot name="header"></slot></div>
-<div class="body"><slot name="body" :expandida="expandida" 
-    :toggleExpandir="toggleExpandir"></slot></div>
-<div class="footer"><slot name="footer">
-    <span>Sin acciones disponibles</span>
-</slot></div>
+<div class="tarjeta">
+    <div class="header"><slot name="header"></slot></div>
+    <div class="body"><slot name="body" :expandida="expandida" 
+        :toggleExpandir="toggleExpandir"></slot></div>
+    <div class="footer"><slot name="footer">
+        <span>Sin acciones disponibles</span>
+    </slot></div>
+</div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-const expandida= ref("false")
+const expandida= ref(false)
 function toggleExpandir () {
     expandida.value =! expandida.value
 }
 
 </script>
 
-<style>
+<style scoped>
 .tarjeta {
   border: 1px solid #ccc;
   padding: 12px;
